6e4af1ecc96a124485dc6e7f25ebd427b900b5b7
robitussin <elizerponiojr@gmail.com>
2023-03-07 13:57:25 +0800

    Update App.java
    Added Ponio Jr

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello Elizer");
+        System.out.println("Hello Elizer Ponio Jr.");
     }
 }
